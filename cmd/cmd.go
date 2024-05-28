6130ba570f391b1fca70ffbc63504787a9548027
chriswolf <chriswolfdesign@gmail.com>
2024-05-28 06:38:07 -0400

    Refactored client behavior into its own struct

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -1,61 +1,10 @@
 package main
 
 import (
-	"bytes"
-	"encoding/gob"
-	"fmt"
-	"net"
+	"tcp-ttt-client/client"
 )
 
-type Message struct {
-	Name string
-	Text string
-}
-
 func main() {
-	conn, err := net.Dial("tcp", "localhost:8080")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	var buf bytes.Buffer
-	enc := gob.NewEncoder(&buf)
-
-	message := Message {
-		Name: "Chris",
-		Text: "Hello",
-	}
-
-	if err = enc.Encode(message); err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	_, err = conn.Write(buf.Bytes())
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	responseBuf := make([]byte, 1024)
-	_, err = conn.Read(responseBuf)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	tmp := bytes.NewBuffer(responseBuf)
-
-	response := &Message{}
-	dec := gob.NewDecoder(tmp)
-
-	if err = dec.Decode(response); err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	fmt.Printf("Received: %+v\n", response)
-
-	conn.Close()
+	cl := client.GenerateClient("Chris", "localhost:8080")
+	cl.ContactServer()
 }
