48b793f00fb9366a9955e4d75f0b5f2144749f9b
chriswolf <chriswolfdesign@gmail.com>
2024-06-01 11:08:45 -0400

    Informs player when it is time to make a move

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"bytes"
+	"encoding/gob"
 	"fmt"
 	"tcp-ttt-client/client"
+
+	"github.com/chriswolfdesign/tcp-ttt-common/strings"
+	"github.com/chriswolfdesign/tcp-ttt-common/tcp_payloads"
 )
 
 func main() {
@@ -13,6 +18,34 @@ func main() {
 
 	fmt.Println("Waiting for game to begin")
 	cl.WaitForGameStart()
+
+	gameOver := false
+
+	for !gameOver {
+		fmt.Println("Game not over")
+
+		playerTurnBuf := make([]byte, 1024)
+		_, err := cl.ServerConn.Read(playerTurnBuf)
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+
+		tmp := bytes.NewBuffer(playerTurnBuf)
+
+		playerTurnMessage := &tcp_payloads.PlayerTurnMessage{}
+		dec := gob.NewDecoder(tmp)
+
+		if err = dec.Decode(playerTurnMessage); err != nil {
+			continue
+		}
+
+		if playerTurnMessage.PayloadType == strings.TYPE_PLAYER_TURN_MESSAGE {
+			fmt.Printf("Player turn is %s!\n", playerTurnMessage.Player)
+		}
+
+		return
+	}
 }
 
 func getPlayerName() string {
